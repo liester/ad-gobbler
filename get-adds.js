eaccb833b4d3142c9bd9ba0170da331790017b30
Levi Liester <levi.liester@gmail.com>
2019-10-13 13:24:45 -0500

    Connect to that database for extra stuffs

--- get-adds.js
+++ get-adds.js
@@ -5,12 +5,13 @@ const moment = require('moment')
 const cheerio = require('cheerio')
 const axios = require('axios')
 const dotenv = require('dotenv')
+const sql = require('mssql')
 dotenv.config();
 
 const query = `SELECT Id, Account_Manager__c,Auction_ID__c, Account_Name__c, Account_Name__r.Name, Account_Name__r.Auction_House_Id__c,Opportunity__r.Auction_Title__c, Opportunity__r.Estimated_Time_Of_Start__c,
 Lot_1__c, Lot_2__c,Lot_3__c,Lot_4__c,Lot_1_Web_Address__c, Lot_2_Web_Address__c, Lot_3_Web_Address__c, Lot_4_Web_Address__c, Start_Date__c 
 FROM Advertising__c
-limit 10`;
+limit 1`;
 
 function querySalesforce(query){
   var conn = new jsforce.Connection({
@@ -91,44 +92,67 @@ function buildCsv(advertisements) {
   })
 }
 
-function queryAuctionLocationAndStartTime(auctionId){
-  return new Promise((resolve, reject)=>{
-    return resolve({
-      location: {
-        streetAddress:'12918 W Pine St',
-        city: 'Omaha',
-        state: 'Nebraska'
-      },
-      startTime: {
-        day: '11',
-        month: '12',
-        year: '2019',
-        hour24: '13',
-        minute: '00',
-        timezone: 'EST'
-      }
-    })
-  })
+
+async function queryAuctionLocationAndStartTime(auctionId){
+  const result = await sql.query`select * from proxibid_reporting.dbo.Auctions where AuctionID = 34`
+
+  const auctionDate = result.recordset[0].columns.AuctionDate;
+  const auctionTime = result.recordset[0].columns.AuctionTime;
+  const auctionStreetAddress = result.recordset[0].columns.StreetAddress;
+  const auctionCit = result.recordset[0].columns.StreetAddress;
+
+  
+
+  return {
+    location: {
+      streetAddress: result.recordset[0].columns.TimeZoneID,
+      city: 'Omaha',
+      state: 'Nebraska'
+    },
+    startTime: {
+      day: '11',
+      month: '12',
+      year: '2019',
+      hour24: '13',
+      minute: '00',
+      timezoneId: result.recordset[0].columns.TimeZoneID
+    }
+  }
+}
+
+async function connectToReportingDatabase(){
+  const database_user = process.env.reporting_database_user;
+  const database_password = process.env.reporting_database_password;
+  // await sql.connect(`jdbc:sqlserver://rptdb;databaseName=proxibid_reporting;user=${database_user};password=${database_password}`);
+  const config = {
+    user: database_user,
+    password: database_password,
+    server: `rptdb`, // You can use 'localhost\\instance' to connect to named instance
+    database: `proxibid_reporting`,
+  }
+  await sql.connect(config)
 }
 
 
 (async function makeItHappen(){
+  await connectToReportingDatabase()
   const advertisements = await querySalesforce(query);
   for( const advertisement of advertisements){
     try {
       console.log(JSON.stringify(advertisement));
-      await downloadLogo(advertisement.Account_Name__r.Auction_House_Id__c,  advertisement.Id,"logo.jpg")
-      await downloadImage(advertisement.Lot_1_Web_Address__c, advertisement.Id, "lot1.jpg")
-      await downloadImage(advertisement.Lot_2_Web_Address__c, advertisement.Id, "lot2.jpg")
-      await downloadImage(advertisement.Lot_3_Web_Address__c, advertisement.Id, "lot3.jpg")
-      await downloadImage(advertisement.Lot_4_Web_Address__c, advertisement.Id, "lot4.jpg")
+      // await downloadLogo(advertisement.Account_Name__r.Auction_House_Id__c,  advertisement.Id,"logo.jpg")
+      // await downloadImage(advertisement.Lot_1_Web_Address__c, advertisement.Id, "lot1.jpg")
+      // await downloadImage(advertisement.Lot_2_Web_Address__c, advertisement.Id, "lot2.jpg")
+      // await downloadImage(advertisement.Lot_3_Web_Address__c, advertisement.Id, "lot3.jpg")
+      // await downloadImage(advertisement.Lot_4_Web_Address__c, advertisement.Id, "lot4.jpg")
       const auctionLocationAndStartTime = await queryAuctionLocationAndStartTime(advertisement.Auction_ID__c)
-      advertisement.auctionLocation = auctionLocationAndStartTime.location;
-      advertisement.startTime = auctionLocationAndStartTime.startTime;
+      // advertisement.auctionLocation = auctionLocationAndStartTime.location;
+      // advertisement.startTime = auctionLocationAndStartTime.startTime;
     } catch (error) {
       console.log(error)
     }
   }
   await buildCsv(advertisements)
+  sql.close()
   console.log(`All dones`)
 })();
